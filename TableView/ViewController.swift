ae4a70a8c2113e58328d7195f03bb126a566fe6c
Daniel Georgas <dgeorgas@students.mchenry.edu>
2024-03-03 10:52:58 -0600

    Commit changes

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -7,13 +7,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var petTable: UITableView!
+    
+    let petArray = ["cat", "dog", "parakeet", "parrot", "canary", "finch", "tropical", "fish", "goldfish", "sea horse", "hamster", "gerbil", "rabbit", "turtle", "snake", "lizard", "hermit crab"]
+    let cellID = "cellID"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        petTable.dataSource = self
+        petTable.delegate = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: cellID)
+        }
+        cell?.textLabel?.text = petArray[indexPath.row]
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedItem = petArray[indexPath.row]
+        let alert = UIAlertController(title: "Your Choice", message: "\(selectedItem)", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {
+            action -> Void in
+        })
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
-
